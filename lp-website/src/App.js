c0f0cce2091ad39f61a18d6f724c03d28836fb80
VisvShah <visvshah1@gmail.com>
2023-03-06 00:52:28 -0500

    About Page

--- lp-website/src/App.js
+++ lp-website/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Navbar from './Pages/Navbar';
+import About from './Pages/About/About';
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <div className = "sections">
+        <About/>
+      </div>
     </div>
   );
 }

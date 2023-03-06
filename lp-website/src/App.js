9d22f12bd26388c535740ad8d9d025a02eda6ea6
VisvShah <visvshah1@gmail.com>
2023-03-06 01:02:15 -0500

    Added routes via react-router-dom

--- lp-website/src/App.js
+++ lp-website/src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
 import Navbar from './Pages/Navbar';
 import About from './Pages/About/About';
+import Contact from './Pages/Contact/Contact';
+import Faq from './Pages/FAQ/Faq';
+import Sponsors from './Pages/Sponsors/Sponsors';
+import Team from './Pages/Team/Team';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <div className = "sections">
-        <About/>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar/>
+        <Routes>
+          <Route path = "/" element = {<About/>}/>
+          <Route path = "/contatcs" element = {<Contact/>}/>
+          <Route path = "/faq" element={<Faq/>} />
+          <Route path = "/sponsors" element = {<Sponsors/>}/>
+          <Route path = "/team" element = {<Team/>}/>
+        </Routes>
+      
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 

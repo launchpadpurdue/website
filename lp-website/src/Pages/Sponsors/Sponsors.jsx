725e89a1f9ea9b6027787246e90202ceca4f58d5
VisvShah <visvshah1@gmail.com>
2023-03-06 00:56:40 -0500

    Pages Setup

---
+++ lp-website/src/Pages/Sponsors/Sponsors.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./sponsors.css"
+
+export default function Sponsors() {
+  return (
+   <div className="sponsors">
+        Sponsors
+   </div>
+  )
+}
---
+++ lp-website/src/Pages/Team/Team.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./team.css"
+
+export default function Team() {
+  return (
+    <div className="team">
+        Team
+    </div>
+  )
+}

5b3fc6f285eaee498bef70c6c6e7363025f7f318
Rohan Purandare <rpuranda@purdue.edu>
2023-04-22 17:22:48 -0400

    added floating application button

--- lp-website/src/App.js
+++ lp-website/src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
+import { useState } from 'react';
 import Navbar from './Pages/Navbar';
 import About from './Pages/About/About';
 import Contact from './Pages/Contact/Contact';
 import Faq from './Pages/FAQ/Faq';
 import Sponsors from './Pages/Sponsors/Sponsors';
 import Team from './Pages/Team/Team';
+import {FaBell} from "react-icons/fa"
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
+  const [buttonText, setButtonText] = useState("Apply to be a LaunchPad Mentee!")
+
+  const mouseOver = () => {
+    setButtonText("Apply to be a LaunchPad Mentee!");
+  }
+
+  const mouseOut = () => {
+    setButtonText(<FaBell></FaBell>);
+  }
+
+  const timeout = setTimeout(function() {
+    setButtonText(<FaBell></FaBell>)
+  }, 3000);
+  
   return (
     <BrowserRouter>
       <div className="App">
@@ -19,7 +35,9 @@ function App() {
           <Route path = "/sponsors" element = {<Sponsors/>}/>
           <Route path = "/team" element = {<Team/>}/>
         </Routes>
-      
+        {timeout}
+        <a href='https://google.com' target="_blank"><button onMouseOver={mouseOver} onMouseOut={mouseOut} className='apply'>{buttonText}</button></a>
+        {/*      ^^^Enter link to the application form here */}
       </div>
     </BrowserRouter>
   );

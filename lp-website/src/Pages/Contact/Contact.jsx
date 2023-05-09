6e203595cb392b83cca27e2f003336cbee7e8d5d
VisvShah <visvshah1@gmail.com>
2023-05-09 18:48:21 -0400

    typeform embed

--- lp-website/src/Pages/Contact/Contact.jsx
+++ lp-website/src/Pages/Contact/Contact.jsx
@@ -4,7 +4,18 @@ import "./contact.css"
 export default function Contact() {
   return (
     <div className="contact">
-        Contacts
+      <div className="top_wrapper">
+        <h1 className="contact_title">Contact Us!</h1>
+        <h2 className="contact_title">Email: launchpadpurdue@gmail.com</h2>
+      </div>
+
+        <iframe
+        src="https://c74ugo6c9ml.typeform.com/to/CjwKajSD"
+        width="100%"
+        height="80%"
+        allow="fullscreen; accelerometer; gyroscope; picture-in-picture"
+        className='contact_form'
+      ></iframe>
     </div>
   )
 }
